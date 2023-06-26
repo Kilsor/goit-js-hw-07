933314e8da4d773054f20ff4a07efbfa00e104ee
Kilsor <kilsors@gmail.com>
2023-06-26 12:40:49 +0300

    hw7.3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,42 +1,70 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
 
-// Generate the markup for each gallery item and add it to the list
-const galleryList = document.querySelector(".gallery");
-galleryList.innerHTML = galleryItems
-  .map(
-    (item) => `
+// Додаємо розмітку елементів галереї на основі масиву даних
+list.insertAdjacentHTML("beforeend", createMarkupItems(galleryItems));
+
+// Функція для створення розмітки елементів галереї
+function createMarkupItems(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
       <li class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
+        <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
-            src="${item.preview}"
-            alt="${item.description}"
+            src="${preview}"
+            alt="${description}"
+            data-source="${original}"
           />
         </a>
-      </li>
-    `
-  )
-  .join("");
+      </li>`
+    )
+    .join("");
+}
 
-// Initialize SimpleLightbox with options
+// Додавання прослуховувачів подій
+list.addEventListener("click", handleImageClick);
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keydown", handleKeyPress);
+
+// Ініціалізація SimpleLightbox
 const lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
+  captions: true, // Включення підписів до зображень
+  captionsData: "alt", // Використання значення атрибуту "alt" для підпису
+  captionPosition: "bottom", // Позиція підпису (bottom, top)
+  captionDelay: 250, // Затримка перед з'явленням підпису (мс)
 });
 
-// Add event listener for 'beforeShow' event to add custom caption styling
-lightbox.on("beforeShow.simplelightbox", function (instance) {
-  const caption = instance.$caption;
-  caption.addClass("custom-caption");
-});
+// Функція-колбек для обробки кліку по зображенню
+function handleImageClick(event) {
+  event.preventDefault(); // Перешкоджаємо переходу за посиланням
 
-// Add event listener for 'shown.simplelightbox' event to remove custom caption styling
-lightbox.on("shown.simplelightbox", function (instance) {
-  const caption = instance.$caption;
-  setTimeout(function () {
-    caption.removeClass("custom-caption");
-  }, 250);
-});
+  if (event.target.classList.contains("gallery__image")) {
+    // Отримуємо посилання на зображення
+    const imageSource = event.target.dataset.source;
+
+    // Відкриття модального вікна з зображенням
+    lightbox.open({ source: imageSource });
+  }
+}
+
+// Функція-колбек для обробки натискання клавіші "Escape" для закриття модального вікна
+function handleKeyDown(event) {
+  if (event.key === "Escape") {
+    lightbox.close(); // Закриття модального вікна
+  }
+}
+
+// Функція-колбек для обробки натискання клавіш "ArrowLeft" та "ArrowRight" для гортання зображень
+function handleKeyPress(event) {
+  if (event.key === "ArrowLeft") {
+    lightbox.prev(); // Перехід до попереднього зображення
+  }
+
+  if (event.key === "ArrowRight") {
+    lightbox.next(); // Перехід до наступного зображення
+  }
+}
