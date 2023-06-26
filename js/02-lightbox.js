20d94ab5458d83c37605f0f0542d0c85411c57c8
Kilsor <kilsors@gmail.com>
2023-06-26 13:03:01 +0300

    hw7.4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -25,11 +25,6 @@ function createMarkupItems(arr) {
     .join("");
 }
 
-// Додавання прослуховувачів подій
-list.addEventListener("click", handleImageClick);
-document.addEventListener("keydown", handleKeyDown);
-document.addEventListener("keydown", handleKeyPress);
-
 // Ініціалізація SimpleLightbox
 const lightbox = new SimpleLightbox(".gallery a", {
   captions: true, // Включення підписів до зображень
@@ -37,34 +32,3 @@ const lightbox = new SimpleLightbox(".gallery a", {
   captionPosition: "bottom", // Позиція підпису (bottom, top)
   captionDelay: 250, // Затримка перед з'явленням підпису (мс)
 });
-
-// Функція-колбек для обробки кліку по зображенню
-function handleImageClick(event) {
-  event.preventDefault(); // Перешкоджаємо переходу за посиланням
-
-  if (event.target.classList.contains("gallery__image")) {
-    // Отримуємо посилання на зображення
-    const imageSource = event.target.dataset.source;
-
-    // Відкриття модального вікна з зображенням
-    lightbox.open({ source: imageSource });
-  }
-}
-
-// Функція-колбек для обробки натискання клавіші "Escape" для закриття модального вікна
-function handleKeyDown(event) {
-  if (event.key === "Escape") {
-    lightbox.close(); // Закриття модального вікна
-  }
-}
-
-// Функція-колбек для обробки натискання клавіш "ArrowLeft" та "ArrowRight" для гортання зображень
-function handleKeyPress(event) {
-  if (event.key === "ArrowLeft") {
-    lightbox.prev(); // Перехід до попереднього зображення
-  }
-
-  if (event.key === "ArrowRight") {
-    lightbox.next(); // Перехід до наступного зображення
-  }
-}
