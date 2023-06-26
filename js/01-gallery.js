0d62b26b1c324e2465a1e5ff1b7d7a1b0ad61009
Kilsor <kilsors@gmail.com>
2023-06-26 10:56:06 +0300

    hw7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Генеруємо розмітку для кожного елемента галереї і додаємо її до списку
+const galleryList = document.querySelector(".gallery");
+galleryList.innerHTML = galleryItems
+  .map(
+    (item) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            data-source="${item.original}"
+          />
+        </a>
+      </li>
+    `
+  )
+  .join("");
+
+// Додаємо обробники подій кліку та натискання на клавішу
+galleryList.addEventListener("click", openModal);
+document.addEventListener("keydown", handleKeyPress);
+
+// Функція для відкриття модального вікна
+function openModal(event) {
+  event.preventDefault();
+  const imageUrl = event.target.dataset.source;
+  if (imageUrl) {
+    modal = basicLightbox.create(`<img src="${imageUrl}">`);
+    modal.show();
+  }
+}
+
+let modal; // Змінна для збереження модального об'єкту
+
+// Функція для обробки натискання на клавішу та закриття модалки
+function handleKeyPress(event) {
+  if (event.code === "Escape" && modal) {
+    modal.close();
+  }
+}
